caf4badff8960a1915c274c2702dacd28703a6e2
juan-pablovm <juanpablovallejosmamani@itibb.edu.bo>
2024-08-07 09:28:43 -0400

    Add files via upload

---
+++ CV/main.js
@@ -0,0 +1,5 @@
+<script
+  src="https://unpkg.com/react-stackai@latest/dist/vanilla/vanilla-stackai.js"
+  data-project-url="https://www.stack-ai.com/embed/cb8a46d8-e279-4ac8-8f68-102ef3e54a67/4eaf831e-b0c1-4506-849a-132e0657dcf0/66aa646d10830939dafe938a">
+</script>
+      
